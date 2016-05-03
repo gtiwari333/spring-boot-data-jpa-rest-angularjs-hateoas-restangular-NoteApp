4a1792332c8aca7bf265a69dfc803eeed0786bb7
Ganesh Tiwari <gtiwari333@gmail.com>
2016-05-03 08:58:46 +0545

    fixed the query findTop3PosterBasedOnPostViewCount to have view_count > 0

--- src/main/java/g/t/note/repo/PersonRepository.java
+++ src/main/java/g/t/note/repo/PersonRepository.java
@@ -14,7 +14,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     @Query(value = "SELECT p.*  FROM person p , note n WHERE p.id = n.person_id GROUP BY n.person_id ORDER BY COUNT(n.id) DESC LIMIT 5", nativeQuery = true)
     List<Person> findTop3PosterBasedOnPostCount();
 
-    @Query(value = "SELECT p.*  FROM person p , note n WHERE p.id = n.person_id GROUP BY n.person_id ORDER BY SUM(n.view_count) DESC LIMIT 5", nativeQuery = true)
+    @Query(value = "SELECT p.*  FROM person p , note n WHERE p.id = n.person_id and n.view_count > 0 GROUP BY n.person_id ORDER BY SUM(n.view_count) DESC LIMIT 5", nativeQuery = true)
     List<Person> findTop3PosterBasedOnPostViewCount();
 
 }
